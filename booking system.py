f0e94887b86b999a6f61cd60105ec53cc3d780fd
mgow1208 <27franklandc@wirralgrammarboys.com>
2025-09-26 22:00:26 +0100

    Update booking system.py

--- booking system.py
+++ booking system.py
@@ -1,14 +1,20 @@
 flights = [[630,1100,1450,1950], [1020,1820], [1250], [1340], [610,1050,1540,2030], [900,1730], [1450], [1020,2100]]
-def price(out, ins
-
-loc = input("Paris, Barcelona, Madrid, Lisbon, Dublin, Belfast, Berlin, Amsterdam? ")
-loc = loc.lower()
-if loc == "paris":
-  out = 0
-  ft = int(input("6:30,11,14:50,19:50 (1,2,3,4)"))
-  ins = ft-1
-
-
+def price(out, ins, money):
+  pe=int(input("how many people, max 6? : "))
+  if flights[out][ins] <901:
+    money = 150*pe
+  elif flights[out][ins]>900 and flights[out][ins]<1700:
+    money = 90*pe
+  elif flights[out][ins]>1659 and flights[out][ins]<2400:
+    money = 120*pe
+  if flights[out]==flights[4] or flights[out]==flights[5]:
+    break
+  else:
+    peifm=int(input("how many people will have in flight meals? Max 6 : "))
+    money = money+(20*peifm)
+  pepb=int(input("how many people will have priority boarding? Max 6 : "))
+  money = money+(30*pepb)
+  return money
 
 def OUT(out):
   loc = int(input("Paris, Barcelona, Madrid, Lisbon, Dublin, Belfast, Berlin, Amsterdam? Enter 1-8: "))
@@ -16,6 +22,6 @@ def OUT(out):
   return out
 
 def INS(out, ins):
-  loc = int(input(f"which? {print(flights[out]} pick from 1-{len(flights[out])} : "))
-  ins = loc-1
+  loc = int(input(f"which? {print(flights[out])} pick from 1-{len(flights[out])} : "))
+  ins = flights[out][loc-1]
   return ins
